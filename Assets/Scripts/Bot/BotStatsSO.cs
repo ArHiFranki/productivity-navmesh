2145ddb5e782af252b6748f4b703590a21055c67
ArHiFranki <franki550@gmail.com>
2022-08-04 00:04:42 +0700

    Added bot stats configuration feature via Scriptable Objects

---
+++ Assets/Scripts/Bot/BotStatsSO.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Productivity.Bot
+{
+    // Bot stats configuration template
+    [CreateAssetMenu(menuName = "Bot Stats", fileName = "New Bot Stats")]
+    public class BotStatsSO : ScriptableObject
+    {
+        [SerializeField] private int moveSpeedMin;
+        [SerializeField] private int moveSpeedMax;
+        [SerializeField] private int damageMin;
+        [SerializeField] private int damageMax;
+        [SerializeField] private int healthMin;
+        [SerializeField] private int healthMax;
+
+        public int MoveSpeedMin => moveSpeedMin;
+        public int MoveSpeedMax => moveSpeedMax;
+        public int DamageMin => damageMin;
+        public int DamageMax => damageMax;
+        public int HealthMin => healthMin;
+        public int HealthMax => healthMax;
+    }
+}
