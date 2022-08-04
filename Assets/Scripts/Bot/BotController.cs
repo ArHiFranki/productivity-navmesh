aced17553cc5b7742788d5ae76e0844badaa8adb
ArHiFranki <franki550@gmail.com>
2022-08-04 14:04:47 +0700

    Implementing action priority

--- Assets/Scripts/Bot/BotController.cs
+++ Assets/Scripts/Bot/BotController.cs
@@ -19,11 +19,11 @@ namespace Productivity.Bot
 
         private void Update()
         {
-            InteractWithCombat();
-            InteractWithMovement();
+            if (InteractWithCombat()) return;
+            if (InteractWithMovement()) return;
         }
 
-        private void InteractWithCombat()
+        private bool InteractWithCombat()
         {
             RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
             foreach (RaycastHit hit in hits)
@@ -35,26 +35,25 @@ namespace Productivity.Bot
                 {
                     GetComponent<Fighter>().Attack(target);
                 }
+                return true;
             }
+            return false;
         }
 
-        private void InteractWithMovement()
-        {
-            if (Input.GetMouseButton(0))
-            {
-                MoveToCursor();
-            }
-        }
-
-        private void MoveToCursor()
+        private bool InteractWithMovement()
         {
             RaycastHit hit;
             bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
 
             if (hasHit)
             {
-                myMover.MoveTo(hit.point);
+                if (Input.GetMouseButton(0))
+                {
+                    myMover.MoveTo(hit.point);
+                }
+                return true;
             }
+            return false;
         }
 
         private static Ray GetMouseRay()
